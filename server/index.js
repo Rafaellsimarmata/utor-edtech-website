f7a346386654e721e8202a4aa40f9238604e67e7
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2023-12-09 06:47:01 +0700

    deploy prod

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(fileUpload({ safeFileNames: true, preserveExtension: true }));
 // app.use(cors({ credentials: true, origin: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://utor-web.vercel.app/');
   // another common pattern
   // res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
