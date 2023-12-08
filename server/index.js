0003821a54f1c870bcf9757cfe471d9056211690
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2023-12-09 06:43:26 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -22,13 +22,25 @@ db.connect((err) => {
 app.use(fileUpload({ safeFileNames: true, preserveExtension: true }));
 
 // To prevent CORS errors
-app.set('trust proxy', 1);
-app.use(cors({ credentials: true, origin: true }));
+// app.set('trust proxy', 1);
+// app.use(cors({ credentials: true, origin: true }));
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // another common pattern
+  // res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   next();
 });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
